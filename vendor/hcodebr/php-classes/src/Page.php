489c8eafbdc7371264952f3f9cc722bfbdc16a87
Pedro Lucas <pedro.lucaswpl@gmail.com>
2020-06-25 16:25:43 -0300

    configurando o login de admin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -8,13 +8,15 @@
         private $tpl;
         private $options = [];
         private $defaults = [
+            "header"=>true,
+            "footer"=>true,
             "data" => []
         ];
-        public function __construct($opts = array())
+        public function __construct($opts = array(),$tpl_dir = "/views/")
         {
             $this-> options = array_merge($this-> defaults, $opts);
             $config = array(
-                "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"] . "/views/",
+                "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"] . $tpl_dir,
                 "cache_dir"     => $_SERVER["DOCUMENT_ROOT"] . "/views-cache/",
                 "debug"         => false
                );
@@ -25,7 +27,7 @@
 
             $this->setData($this-> options["data"]);
 
-            $this-> tpl->draw("header");
+            if ($this-> options ["header"] === true) $this-> tpl->draw("header");
 
         }
 
@@ -41,14 +43,14 @@
         {
             $this-> setData($data);
 
-            $this-> tpl->draw("index");
+            $this-> tpl->draw($name);
         }
 
 
         public function __destruct()
         {
             
-            $this-> tpl-> draw("footer");
+            if ($this-> options ["footer"] === true) $this-> tpl-> draw("footer");
 
         }
     }
