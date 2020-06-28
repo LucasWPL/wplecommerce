2b72936ff6da07fb58402971cb8439457e9d3421
Pedro Lucas <pedro.lucaswpl@gmail.com>
2020-06-27 21:41:16 -0300

    crud admin/products

---
+++ admin-category.php
@@ -0,0 +1,104 @@
+<?php
+
+use \Wpl\PageAdmin;
+use \Wpl\Models\User;
+use \Wpl\Models\Category;
+
+$app-> get ("/admin/categories",function(){
+
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page -> setTpl("categories",[
+		"categories"=>$categories
+	]);
+	exit; 
+});
+
+$app-> get ("/admin/categories/create",function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page -> setTpl("categories-create");
+	exit; 
+});
+
+$app-> post ("/admin/categories/create",function(){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+});
+
+$app-> get ("/admin/categories/:idcategory/delete",function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category -> get((int)$idcategory);
+
+	$category ->delete();
+
+	header("Location: /admin/categories");
+	exit;
+});
+
+$app-> get ("/admin/categories/:idcategory",function($idcategory){
+	
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category -> get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page -> setTpl("categories-update",[
+		"category"=>$category->getData()
+	]);
+});
+
+$app-> post ("/admin/categories/:idcategory",function($idcategory){
+	
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category -> get((int)$idcategory);
+
+	$category -> setData($_POST);
+
+	$category -> save();
+
+	header("Location: /admin/categories");
+	exit;
+});
+
+$app-> get ("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+
+	$category -> get((int)$idcategory);
+
+	$page = new Page();
+
+	$page -> setTpl("category", [
+		'category'=> $category->getData(),
+		'products'=> []
+	]);
+});
+
+?>
