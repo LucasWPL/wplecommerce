805fdef79d0c381650f715c6a3fea4fd7975c107
Pedro Lucas <pedro.lucaswpl@gmail.com>
2020-07-02 20:38:02 -0300

    paginação e search admin pedidos

--- admin-orders.php
+++ admin-orders.php
@@ -78,11 +78,41 @@ $app-> get("/admin/orders",function(){
     
     User::verifyLogin();
 
-    $page = new PageAdmin();
-    
-    $page-> setTpl('orders',[
-        'orders'=>Order::listAll()
-    ]);
+    $order = new Order();
+
+    $search = (isset($_GET['search'])) ? $_GET['search'] : '';
+	$numpage = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+	
+	if($search != ''){
+
+		$pagination = Order::getPageSearch($search, $numpage);
+
+
+	}else{
+
+		$pagination = Order::getPage($numpage);
+
+	}
+
+	$pages = [];
+
+	for($x = 0; $x < $pagination['pages']; $x++){
+		array_push($pages,[
+			'href'=> '/admin/orders?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+			]),
+			'text'=>$x+1
+		]);
+	}
+
+	$page = new PageAdmin();
+
+	$page -> setTpl("orders",[
+		"orders"=>$pagination['data'],
+		'search'=>$search,
+		'pages'=>$pages
+	]);
 });
 
 
--- vendor/hcodebr/php-classes/src/Models/Order.php
+++ vendor/hcodebr/php-classes/src/Models/Order.php
@@ -132,6 +132,64 @@ class Order extends Model
 		$_SESSION[Order::SUCCESS] = NULL;
 
     }
+
+    public static function getPage($page = 1, $itemsPerPage = 10)
+    {
+        $start = ($page - 1) * $itemsPerPage;
+    
+        $sql = new Sql();
+
+        $res = $sql -> select(
+        "SELECT SQL_CALC_FOUND_ROWS *
+        FROM tb_orders a 
+            INNER JOIN tb_ordersstatus b USING(idstatus) 
+            INNER JOIN tb_carts c USING(idcart)
+            INNER JOIN tb_users d ON d.iduser = a.iduser
+            INNER JOIN tb_addresses USING(idaddress)
+            INNER JOIN tb_persons f ON f.idperson = d.idperson
+            ORDER BY a.dtregister DESC
+        LIMIT $start, $itemsPerPage;
+        ");
+
+        $resTotal = $sql-> select("SELECT FOUND_ROWS() AS nrtotal;");
+
+        return [
+            'data'=> $res,
+            'total'=> (int)$resTotal[0]['nrtotal'],
+            'pages'=> ceil($resTotal[0]['nrtotal'] / $itemsPerPage)
+        ];
+    }
+
+    public static function getPageSearch($search, $page = 1, $itemsPerPage = 10)
+    {
+        $start = ($page - 1) * $itemsPerPage;
+    
+        $sql = new Sql();
+
+        $res = $sql -> select(
+        "SELECT SQL_CALC_FOUND_ROWS *
+        FROM tb_orders a 
+            INNER JOIN tb_ordersstatus b USING(idstatus) 
+            INNER JOIN tb_carts c USING(idcart)
+            INNER JOIN tb_users d ON d.iduser = a.iduser
+            INNER JOIN tb_addresses USING(idaddress)
+            INNER JOIN tb_persons f ON f.idperson = d.idperson
+            WHERE a.idorder = :id OR f.desperson LIKE :search
+            ORDER BY a.dtregister DESC
+        LIMIT $start, $itemsPerPage;
+        ",[
+            ':search'=> '%'.$search.'%',
+            ':id'=> $search
+        ]); 
+
+        $resTotal = $sql-> select("SELECT FOUND_ROWS() AS nrtotal;");
+
+        return [
+            'data'=> $res,
+            'total'=> (int)$resTotal[0]['nrtotal'],
+            'pages'=> ceil($resTotal[0]['nrtotal'] / $itemsPerPage)
+        ];
+    }
     
 }
 
